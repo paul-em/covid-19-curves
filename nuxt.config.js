73bfb7b2b6e78eb088051e6417e00f93e340f952
Paul Em <paul3m@gmail.com>
2020-03-17 15:37:39 +0100

    add nuxt config options for gh pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,17 @@
 const pkg = require('./package');
 
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/covid-19-curves/',
+  },
+} : {};
+
+
 module.exports = {
   mode: 'spa',
 
+  ...routerBase,
   /*
   ** Headers of the page
   */
