a723bbe878db531031513b523155fb7c7da5cc28
Paul Em <paul3m@gmail.com>
2020-03-21 01:04:38 +0100

    add offset sliders

--- components/LineChart.vue
+++ components/LineChart.vue
@@ -7,6 +7,22 @@
         height="400"
       />
     </div>
+    <div class="p-2">
+      <div
+        v-for="dataset in datasets"
+        :key="dataset.label"
+        class="p-1">
+        Offset for {{ dataset.label }}:
+        <input
+          :value="getOffset(dataset.label)"
+          type="range"
+          name="vol"
+          min="-50"
+          max="50"
+          @input="e => updateOffset(dataset.label, e.target.value)">
+        {{ getOffset(dataset.label) }} Days
+      </div>
+    </div>
     <button
       class="p-3 my-2 text-sm hover:bg-grey-lighter rounded-sm uppercase"
       @click="toggleViewType">
@@ -26,6 +42,7 @@ export default {
   data: () => ({
     chart: null,
     linear: true,
+    offsets: {},
   }),
   watch: {
     datasets: {
@@ -62,6 +79,44 @@ export default {
       this.linear = !this.linear;
       this.chart.options.scales.yAxes[0].type = this.linear ? 'linear' : 'logarithmic';
       this.chart.update();
+    },
+    updateOffset(label, v) {
+      console.log('updateOffset', label, v);
+      this.offsets = {
+        ...this.offsets,
+        [label]: v,
+      };
+      this.update();
+    },
+    update() {
+      this.chart.data.datasets = this.datasets.map(dataset => ({
+        ...dataset,
+        data: this.shift(dataset.data, this.offsets[dataset.label]),
+      }));
+      this.chart.update();
+    },
+    getOffset(label) {
+      return this.offsets[label] || 0;
+    },
+    shift(arr, offset) {
+      if (offset === 0) {
+        return arr;
+      }
+      const filler = [];
+      for (let i = 0; i < Math.abs(offset); i += 1) {
+        filler.push(0);
+      }
+      if (offset < 0) {
+        const newArr = [...arr].slice(Math.abs(offset));
+        // newArr.push(...filler);
+        return newArr;
+      }
+      const newArr = [
+        ...filler,
+        ...arr,
+      ].slice(0, arr.length);
+      console.log(newArr);
+      return newArr;
     },
   },
 };
