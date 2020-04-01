7f4468ea027fee5e2b0a2f6327c71ac5a8c2fce2
Paul Em <paul3m@gmail.com>
2020-04-01 03:42:46 +0200

    adjust page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'COVID-19 Curves',
+    title: 'Corona Virus Curves',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
